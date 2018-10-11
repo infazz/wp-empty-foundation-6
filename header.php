f61de42e40d2f56ad8f92ce844b684c9696fb1a0
infazz <gleb@blueglass.ee>
2018-10-11 14:37:41 +0300

    offcanvas update

--- header.php
+++ header.php
@@ -9,53 +9,51 @@
 <head>
 
 	<meta charset="<?php bloginfo('charset'); ?>">
-	
+
 	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
+
 	<?php if (is_search()) { ?>
-		<meta name="robots" content="noindex, nofollow" /> 
+		<meta name="robots" content="noindex, nofollow" />
 	<?php } ?>
 
 	<title><?php wp_title(); ?></title>
-	
-	
+
+
 	<meta name="google-site-verification" content="">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-	
+
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/i/favicon.ico">
-	
+
 
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
 	<?php wp_head(); ?>
-	
+
 </head>
 
 <body <?php body_class(); ?>>
 
-<div class="off-canvas-wrap" data-offcanvas>
-  	<div class="inner-wrap">
 
-    <a class="left-off-canvas-toggle" href="#" >Menu</a>
+<div class="off-canvas-wrapper">
 
-    <!-- Off Canvas Menu -->
-    <aside class="left-off-canvas-menu">
-        <!-- whatever you want goes here -->
-        <ul>
-          <li><a href="#">Item 1</a></li>
-        ...
-        </ul>
-    </aside>
-	
-		<div id="page-wrap">
+	<div class="off-canvas position-left " id="leftOffCanvas" data-off-canvas>
+		<ul>
+			<li><a href="#">Item 1</a></li>
+		</ul>
+	</div>
 
-			<header id="header">
-				<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-				<div class="description"><?php bloginfo('description'); ?></div>
+	<div class="off-canvas-content" data-off-canvas-content>
 
-				
-				<?php wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'main-nav', 'menu_class' => 'nav', 'theme_location' => 'top-menu' ) ); ?>
-			</header>
+	    <button type="button" class="hamburger" data-toggle="leftOffCanvas">Open Menu</button>
 
+			<div id="page-wrap">
+
+				<header id="header">
+					<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
+					<div class="description"><?php bloginfo('description'); ?></div>
+
+
+					<?php wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'main-nav', 'menu_class' => 'nav', 'theme_location' => 'top-menu' ) ); ?>
+				</header>
